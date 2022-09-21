99af5887dee4b9506d64aa3c8f47cbb590f33784
qctest <baotg2@younetgroup.com>
2022-09-21 14:07:26 +0700

    add test script on Event App- Friends

--- src/test/java/MetaFox/TestRunnerMetaFoxV5.java
+++ src/test/java/MetaFox/TestRunnerMetaFoxV5.java
@@ -17,7 +17,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = {"src/test/java/MetaFox/testFeature"},
         glue = "MetaFox",
-        tags = "@AdminConfigAllowPostCoverPhotoToFeed",
+        tags = "@BrianAddNewEventsOfflineIsFriends",
         plugin ={
                 "html:target/result/",
                 "pretty",
