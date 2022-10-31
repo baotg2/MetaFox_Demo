7ce72fbeca5b0543fedc210160b79189c77a1d72
jacky158 <jacky@phpfox.com>
2022-11-01 06:18:27 +0700

    fix: update allure project_id

--- .env.example.sh
+++ .env.example.sh
@@ -2,9 +2,9 @@
 
 export BROWSERSTACK_USERNAME=
 export BROWSERSTACK_ACCESS_KEY=
-export ALLURE_SERVER="https://cloudcall-s01.phpfox.com/allure-docker-service"
-export ALLURE_PROJECT=metafox-e2e-dev
-export BASE_URL="https://dev-foxsocial.phpfox.us/"
+export ALLURE_SERVER="https://cloudcall-s01.phpfox.com"
+export ALLURE_PROJECT=e2e
+export BASE_URL="https://dev-foxsocial.phpfox.us"
 
 # Run command to load new environment variables
 # source ./env.example.sh
--- env.sh
+++ env.sh
@@ -2,10 +2,10 @@
 
 export BROWSERSTACK_USERNAME="baotran_moYtnK"
 export BROWSERSTACK_ACCESS_KEY="2cnCJbmwkKAmmb9hEVsN"
-#export ALLURE_SERVER="https://cloudcall-s01.phpfox.com/allure-docker-service"
-#export ALLURE_PROJECT=metafox-e2e-dev
+#export ALLURE_SERVER="https://cloudcall-s01.phpfox.com"
+#export ALLURE_PROJECT=e2e
 export BASE_URL="http://localhost:3000"
 export GITHUB_PERSONAL_TOKEN="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"
 export ALLURE_SERVER="http://localhost"
-export ALLURE_PROJECT=frontend
+export ALLURE_PROJECT=e2e
 
