2c3d0f79cf47edce61842cf074282482d062db6c
jacky158 <jacky@phpfox.com>
2022-11-01 06:24:00 +0700

    migrate allure project_id to

--- env.sh
+++ env.sh
@@ -7,5 +7,5 @@ export BROWSERSTACK_ACCESS_KEY="2cnCJbmwkKAmmb9hEVsN"
 export BASE_URL="http://localhost:3000"
 export GITHUB_PERSONAL_TOKEN="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"
 export ALLURE_SERVER="http://localhost"
-export ALLURE_PROJECT=e2e
+export ALLURE_PROJECT=default
 
