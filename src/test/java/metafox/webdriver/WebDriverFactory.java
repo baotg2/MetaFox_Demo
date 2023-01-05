f47fb915f3e905310ff1a0057ddef54484608171
Vincent <103025986+baotg2@users.noreply.github.com>
2023-01-05 09:48:45 +0700

    Update WebDriverFactory.java

--- src/test/java/metafox/webdriver/WebDriverFactory.java
+++ src/test/java/metafox/webdriver/WebDriverFactory.java
@@ -89,7 +89,6 @@ public class WebDriverFactory {
             String URL = String.format("https://%s/wd/hub", testConfig.get("server"));
 
             DesiredCapabilities caps = new DesiredCapabilities(platform);
-            caps.setCapability("browserstack.idleTimeout", "300");
             return new RemoteWebDriver(new URL(URL), caps);
         } catch (MalformedURLException var4) {
             throw new Error("Unable to create WebDriver", var4);
