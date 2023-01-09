a451dfd100e7b7f801b662708d2ce09936fe303f
Vincent <103025986+baotg2@users.noreply.github.com>
2023-01-09 11:39:13 +0700

    change github work flow

--- src/test/java/metafox/webdriver/WebDriverFactory.java
+++ src/test/java/metafox/webdriver/WebDriverFactory.java
@@ -89,8 +89,6 @@ public class WebDriverFactory {
             String URL = String.format("https://%s/wd/hub", testConfig.get("server"));
 
             DesiredCapabilities caps = new DesiredCapabilities(platform);
-            caps.setCapability("browserstack.local", "true");
-            caps.setCapability("browserstack.idleTimeout", "200");
 
             return new RemoteWebDriver(new URL(URL), caps);
         } catch (MalformedURLException var4) {
