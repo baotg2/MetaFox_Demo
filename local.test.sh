0bd460042613ab05519b991191227ce515186180
Vincent <103025986+baotg2@users.noreply.github.com>
2023-01-11 09:00:33 +0700

    change dispatch

--- local.test.sh
+++ local.test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mvn clean
-mvn test -Dcucumber.filter.tags="@registerNewUser" -Dconf="local" -Ddataproviderthreadcount=1 ##-Dheadless=true
+mvn test -Dcucumber.filter.tags="@previewSite" -Dconf="local" -Ddataproviderthreadcount=1 -Dheadless=true
